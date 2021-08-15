65eb73783f79325e5c0fa71d9e5c1c152b5d9da7
Steffen Dohse <steffen.dohse@wob-racing.de>
2021-08-15 13:31:33 +0800

    Added the first install.py script Changed the readme to the new script

---
+++ install.py
@@ -0,0 +1,6 @@
+config_file = open('/media/steffen/boot/config.txt', 'a')
+config_file.write('\nhdmi_force_hotplug=1\n')
+config_file.write('hdmi_group=2\n')
+config_file.write('hdmi_mode=87\n')
+config_file.write('hdmi_cvt=800 480 60 6 0 0 0\n')
+config_file.write('hdmi_drive=1\n')

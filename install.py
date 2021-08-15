5dbb5305f2871f6da2d1e6fabd81b898ba9bedbb
Steffen Dohse <steffen.dohse@wob-racing.de>
2021-08-15 20:15:07 +0800

    yet another install.py update

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from time import sleep
 import os
 from configparser import ConfigParser
@@ -8,14 +9,14 @@ config_file.read("config.ini")
 firstboot = config_file.getboolean('INSTALL', 'firstboot')
 
 if firstboot:
-    config_file = open('/boot/config.txt', 'a')
-    config_file.write('\nhdmi_force_hotplug=1\n')
-    config_file.write('hdmi_group=2\n')
-    config_file.write('hdmi_mode=87\n')
-    config_file.write('hdmi_cvt=800 480 60 6 0 0 0\n')
-    config_file.write('hdmi_drive=1\n')
-    config_file['INSTALL']['firstboot']
-    with open('config.ini', 'w') as config_file:    # save
-        config.write(config_file)
+    boot_file = open('/boot/config.txt', 'a')
+    boot_file.write('\nhdmi_force_hotplug=1\n')
+    boot_file.write('hdmi_group=2\n')
+    boot_file.write('hdmi_mode=87\n')
+    boot_file.write('hdmi_cvt=800 480 60 6 0 0 0\n')
+    boot_file.write('hdmi_drive=1\n')
+
+    config_file.set('INSTALL', 'firstboot', 'False')
+    config_file.write(open("config.ini", "w"))
     sleep(1)
     os.system('sudo shutdown -r now')

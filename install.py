fee3d82540089215108e4d84e8adab8ee4c533cd
Steffen Dohse <steffen.dohse@wob-racing.de>
2021-08-15 21:14:06 +0800

    Added wpa_supplicant config

--- install.py
+++ install.py
@@ -16,6 +16,11 @@ if firstboot is True:
     boot_file.write('hdmi_cvt=800 480 60 6 0 0 0\n')
     boot_file.write('hdmi_drive=1\n')
 
+    wpa_file = open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a')
+    wlannetzwerkname = '1504'
+    wlanpasswort = '1234567890'
+    wpa_file.write('\nnetwork={\n       ssid="'+wlannetzwerkname+'"\n       psk="'+wlanpasswort+'"\n       key_mgmt=WPA-PSK\n}')
+
     config_file.set('GENERAL', 'firstboot', 'False')
     config_file.write(open('/home/pi/soaribox/config.ini', 'w'))
     sleep(1)

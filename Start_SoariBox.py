1a202743c0a174393f79c9000fc65160c8c131d6
sunlord2k <steffen-dohse@web.de>
2022-06-24 15:19:47 +0800

    Updated start

--- Start_SoariBox.py
+++ Start_SoariBox.py
@@ -9,8 +9,14 @@ def startxcsoar(*args):
     os.system('/usr/bin/xcsoar')
 
 
+def startxserver(*args):
+    os.system('sudo /usr/bin/X11/X -nolisten tcp -dpi 96 :0 > /dev/null 2>&1')
+
+
+startxserver()
+menu.menustart()
 xcsoar = threading.Thread(target=startxcsoar, args=(0,))
 xcsoar.start()
-startdatahandler = threading.Thread(target=datahandler.starthandler)
-startdatahandler.start()
+# startdatahandler = threading.Thread(target=datahandler.starthandler)
+# startdatahandler.start()
 print('This is just a test!')

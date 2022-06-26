7dae3bf587c130923649295233536592394812e3
sunlord2k <steffen-dohse@web.de>
2022-06-26 20:04:50 +0800

    Changes C Server startup

--- Start_SoariBox.py
+++ Start_SoariBox.py
@@ -10,13 +10,19 @@ def startxcsoar(*args):
     os.system('/usr/bin/xcsoar')
 
 
+def startmenux(*args):
+    os.system('sudo xinit /usr/bin/python3 /home/pi/soaribox/Menu.py ')
+
+
+startmenux()
+startdatahandler = threading.Thread(target=datahandler.starthandler)
+startdatahandler.start()
+startxcsoar()
 # os.system('/usr/bin/X11/X -nolisten tcp -dpi 96 :0 > /dev/null 2>&1')
 # sleep(10)
-menu.loadconfig()
-menu.startmenu()
-# os.system('killall Xorg')
+# menu.loadconfig()
+# menu.startmenu()
 # xcsoar = threading.Thread(target=startxcsoar, args=(0,))
 # xcsoar.start()
-# startdatahandler = threading.Thread(target=datahandler.starthandler)
-# startdatahandler.start()
+
 print('This is just a test!')

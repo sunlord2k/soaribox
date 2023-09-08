f4cf0745ecd07f4a0d817d2e891f5cbc5f88122c
sunlord2k <steffen-dohse@web.de>
2023-09-08 22:31:35 +0200

    123

--- powermanagement.py
+++ powermanagement.py
@@ -2,13 +2,22 @@
 # -*- coding:utf-8 -*-
 import smbus
 import time
+import psutil
 
 address = 0x61
 
+
+def is_process_running(name):
+    for process in psutil.process_iter(['name']):
+        if process.info['name'] == name:
+            return True
+    return False
+
+
 bus = smbus.SMBus(1)
 while True:
-    bus.write_byte(address, 0x33)
     time.sleep(1)
-    read = bus.read_byte(address)
-    read = hex(read)
+    read = hex(bus.read_byte(address))
     print(read)
+    if not is_process_running('xcsoar'):
+        bus.write_byte(address, 0x34)

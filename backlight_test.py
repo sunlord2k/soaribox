b76bd322eb240ca6494f6382ad876f579fdafdef
sunlord2k <steffen-dohse@web.de>
2023-10-07 22:02:35 +0200

    3

--- backlight_test.py
+++ backlight_test.py
@@ -13,7 +13,7 @@ GPIO.setup(ledpin, GPIO.OUT)
 
 
 def setbrightness(dutycycle, *args):
-	pi_pwm = GPIO.PWM(ledpin, 20)		# create PWM instance with frequency
+	pi_pwm = GPIO.PWM(ledpin, 1000)		# create PWM instance with frequency
 	pi_pwm.start(dutycycle)					# start PWM of required Duty Cycle
 
 

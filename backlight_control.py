7869349725f65b0c70ce6398f02006d9be6bb775
sunlord2k <steffen-dohse@web.de>
2023-10-07 20:38:57 +0200

    backlight control with rotary

--- backlight_control.py
+++ backlight_control.py
@@ -4,29 +4,34 @@ http://www.electronicwings.com
 '''
 
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-def setbrightness(tmp,*args):
-	ledpin = 13				# PWM pin connected to LED
-	GPIO.setwarnings(True)			#disable warnings
-	GPIO.setmode(GPIO.BCM)			#set pin numbering system
-	GPIO.setup(ledpin,GPIO.OUT)
-	pi_pwm = GPIO.PWM(ledpin, 500)		#create PWM instance with frequency
-	pi_pwm.start(tmp)				#start PWM of required Duty Cycle
-#    pi_pwm.ChangeDutyCycle(0)
-#    while True:
-#        for duty in range(0,101,1):
-#            pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-#            sleep(0.01)
-#            print(duty)
-#        sleep(0.5)
-#
-#        for duty in range(100,-1,-1):
-#            pi_pwm.ChangeDutyCycle(duty)
-#            sleep(0.01)
-#            print(duty)
-#        sleep(0.5)
+ledpin = 13				# PWM pin connected to LED
+rotary_dt = 4
+rotary_clk = 5
+rotary_sw = 22
+GPIO.setwarnings(True)				# disable warnings
+GPIO.setmode(GPIO.BCM)				# set pin numbering system
+GPIO.setup(ledpin, GPIO.OUT)
+GPIO.setup(rotary_sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def setbrightness(dutycycle, *args):
+	pi_pwm = GPIO.PWM(ledpin, 500)		# create PWM instance with frequency
+	pi_pwm.start(dutycycle)					# start PWM of required Duty Cycle
+
+
+def donothing():
+	pass
+
+
+def printstuff():
+	print(GPIO.input(rotary_sw))
+	time.sleep(2)
+
+
 if __name__ == '__main__':
 	setbrightness(10)
 	while True:
-		i=0
+		donothing()
+		printstuff()

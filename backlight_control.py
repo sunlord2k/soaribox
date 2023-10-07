1361a2a65f9b3e4707203cd0510211e6646d42eb
sunlord2k <steffen-dohse@web.de>
2023-10-07 22:19:50 +0200

    123

--- backlight_control.py
+++ backlight_control.py
@@ -19,10 +19,6 @@ GPIO.setup(rotary_dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rotary_clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-def setbrightness(dutycycle, *args):
-	pi_pwm.ChangeDutyCycle(dutycycle)					# start PWM of required Duty Cycle
-
-
 def donothing():
 	pass
 
@@ -38,7 +34,7 @@ def printstuff():
 def main(*args):
 	pi_pwm = GPIO.PWM(ledpin, 1000)		# create PWM instance with frequency
 	pi_pwm.start(100)
-	setbrightness(100)
+	pi_pwm.ChangeDutyCycle(100)
 	counter = 0
 	clkLastState = GPIO.input(rotary_clk)
 	try:
@@ -53,7 +49,7 @@ def main(*args):
 					if counter > 0:
 						counter -= 5
 				print(counter)
-				setbrightness(100 - counter)
+				pi_pwm.ChangeDutyCycle(100 - counter)
 			clkLastState = clkState
 			time.sleep(0.001)
 	finally:

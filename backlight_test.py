aac21d28a1643e76919d45629a78723a703a9b5c
sunlord2k <steffen-dohse@web.de>
2023-10-07 22:01:18 +0200

    added backlighttest

---
+++ backlight_test.py
@@ -0,0 +1,25 @@
+'''
+Control the Brightness of LED using PWM on Raspberry Pi
+http://www.electronicwings.com
+'''
+
+import RPi.GPIO as GPIO
+
+ledpin = 13				# PWM pin connected to LED
+
+GPIO.setwarnings(True)				# disable warnings
+GPIO.setmode(GPIO.BCM)				# set pin numbering system
+GPIO.setup(ledpin, GPIO.OUT)
+
+
+def setbrightness(dutycycle, *args):
+	pi_pwm = GPIO.PWM(ledpin, 20)		# create PWM instance with frequency
+	pi_pwm.start(dutycycle)					# start PWM of required Duty Cycle
+
+
+def donothing():
+	pass
+
+
+if __name__ == '__main__':
+	setbrightness(100)

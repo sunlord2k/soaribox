a4b9c23181b881ffad820d34f88023711d8dbf05
sunlord2k <steffen-dohse@web.de>
2023-10-24 21:02:02 +0200

    try catch error on powermanagement

--- powermanagement.py
+++ powermanagement.py
@@ -19,14 +19,25 @@ def donothing():
     pass
 
 
-bus = smbus.SMBus(1)
-while True:
+def readbus():
     time.sleep(1)
     read = hex(bus.read_byte(address))
     match read:
+        case "0x31":
+            print('Received 0x31')
         case "0x33":
             donothing()
             print('Received 0x33')
         case "0x34":
             os.system('sudo halt -p')
             print('received 0x34')
+
+
+bus = smbus.SMBus(1)
+while True:
+    try:
+        readbus()
+        break
+    except Exception as e:
+        print("Function errored out!", e)
+        print("Retrying ... ")
